ac7108ee34ae140394cae6be170680cffd3c4d73
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-10 01:33:26 +0200

    Creating the Tab component.

---
+++ src/components/tab/Tab.jsx
@@ -0,0 +1,36 @@
+import { memo } from "react"
+import { NavLink, useLocation } from "react-router-dom"
+import PropTypes from "prop-types"
+import "./tab.css"
+
+const Tab = memo(function Tab({tabLinks}) {
+
+    const location = useLocation()
+    
+    return (
+        <div className="tab">
+            <ul className="tab__list">
+            {
+                tabLinks.map((tabLink) => {
+                    return (
+                        <li className="tab__list-item" key={tabLink.name} >
+                            <NavLink className={location.pathname === tabLink.path ? "tab__link tab__link--active" : "tab__link"} to={tabLink.path} >
+                                {tabLink.name}
+                            </NavLink>
+                        </li>
+                    )
+                })
+            }
+            </ul>
+        </div>
+    )
+})
+
+Tab.propTypes = {
+    tabLinks: PropTypes.any.isRequired
+}
+
+export default Tab
+
+
+
