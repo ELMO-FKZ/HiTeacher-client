4009016de7fb59cecddc5811da3ac7aa71f248e6
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-10 01:25:10 +0200

    Creating a context for top and side bar menus.

---
+++ src/contexts/TopSideBarsContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect, useMemo } from "react"
+import PropTypes from "prop-types"
+
+export const TopSideBarsContext = createContext()
+
+export const TopSideBarsContextProvider = ({ children }) => {
+
+    const [isShrinking, setIsShrinking] = useState(false)
+    const [isSideBarShown, setIsSideBarShown] = useState(false)
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth < 768) {
+                setIsShrinking(false)
+            }
+        }
+        handleResize()
+        window.addEventListener("resize", handleResize)
+        return () => window.removeEventListener("resize", handleResize)
+    }, [])
+
+    const contextValues = useMemo(() => {
+        return { isShrinking, setIsShrinking , isSideBarShown, setIsSideBarShown }
+    }, [ isShrinking, setIsShrinking, isSideBarShown, setIsSideBarShown ])
+
+    return (
+        <TopSideBarsContext.Provider value={contextValues}>
+            {children}
+        </TopSideBarsContext.Provider>
+    )
+}
+
+TopSideBarsContextProvider.propTypes = {
+    children: PropTypes.any.isRequired
+}
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,9 @@ import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
+      <TopSideBarsContextProvider>
     <App />
+      </TopSideBarsContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 )
