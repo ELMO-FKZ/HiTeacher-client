430596bc2d31d3f60d5bcdab89aa1998b078e68d
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-09-10 02:14:50 +0200

    Creating the widget component.

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,23 @@
+import { memo } from "react"
+import PropTypes from "prop-types"
+
+const Widget = memo(function Widget({icon, indicator, title}) {
+
+return (
+    <div className="dashboard__card">
+        {icon}
+        <div className="dashboard__card-info">
+            <div className="dashboard__card-number">{indicator}</div>
+            <div className="dashboard__card-title">{title}</div>
+        </div>
+    </div>
+)
+})
+
+export default Widget
+
+Widget.propTypes = {
+    icon: PropTypes.any.isRequired,
+    indicator: PropTypes.any.isRequired,
+    title: PropTypes.any.isRequired
+}
