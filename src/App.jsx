c4feac3af73f316d868509d33e9529ccf3877f05
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-12-23 10:46:12 +0100

    organizing routes in a separate file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,9 @@
 import { useEffect, useContext } from "react"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { ClassesContext } from "./contexts/ClassesContext"
 import { StudentsContext } from "./contexts/StudentsContext"
-import { useAuthContext } from "./hooks/useAuthContext"
-import Layout from "./components/layout/Layout"
-import Dashboard from "./pages/dashboard/Dashboard"
-import Profile from "./pages/profile/Profile"
-import NewProfile from "./pages/newProfile/NewProfile"
-import Classes from "./pages/classes/Classes"
-import NewClass from "./pages/newClass/NewClass"
-import EditClass from "./pages/editClass/EditClass"
-import Students from "./pages/students/Students"
-import NewStudent from "./pages/newStudent/NewStudent"
-import EditStudent from "./pages/editStudent/EditStudent"
-import ViewStudent from "./pages/viewStudent/ViewStudent"
-import Attendance from "./pages/attendance/Attendance"
-import NewAttendance from "./pages/newAttendance/NewAttendance"
-import DeleteAttendance from "./pages/deleteAttendance/DeleteAttendance"
-import Behaviour from "./pages/behaviour/Behaviour"
-import NewBehaviour from "./pages/newBehaviour/NewBehaviour"
-import ViewBehaviour from "./pages/viewBehaviour/ViewBehaviour"
-import EditBehaviour from "./pages/editBehaviour/EditBehaviour"
-import Login from "./pages/login/Login"
-import NotFound from "./pages/notFound/NotFound"
-import PropTypes from "prop-types"
+import AppRoutes from "./routes/AppRoutes"
 
 function App() {
-
-  const { user } = useAuthContext()
-  const RequireAuth = ({ children }) => {
-    return user ? children : <Navigate to="/login" />
-  }
-  const NoRequireAuth = ({children}) => {
-    return !user ? children : <Navigate to="/" />
-  }
-  const AdminAuth = ({ children }) => {
-    return user?.user.role == "Admin" ? children : <Navigate to="/profile" />
-  }
 
   const { getClasses } = useContext(ClassesContext)
   const { getStudents } = useContext(StudentsContext)
@@ -50,48 +17,8 @@ function App() {
   }, [])
 
   return (
-    <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RequireAuth><Layout /></RequireAuth>}>
-          <Route index element={<RequireAuth><Dashboard /></RequireAuth>} />
-          <Route path="profile" >
-            <Route index element={<RequireAuth><Profile /></RequireAuth>} />
-            <Route path="new" element={<AdminAuth><NewProfile /></AdminAuth>} />
-          </Route>
-          <Route path="classes" >
-            <Route index element={<RequireAuth><Classes/></RequireAuth>} />
-            <Route path="new" element={<RequireAuth><NewClass /></RequireAuth>} /> 
-            <Route path="edit/:name" element={<RequireAuth><EditClass /></RequireAuth>} /> 
-          </Route>
-          <Route path="students" >
-            <Route index element={<RequireAuth><Students /></RequireAuth>} />
-            <Route path="new" element={<RequireAuth><NewStudent /></RequireAuth>} />
-            <Route path="view/:id" element={<RequireAuth><ViewStudent /></RequireAuth>} />
-            <Route path="edit/:id/:code" element={<RequireAuth><EditStudent /></RequireAuth>} />
-          </Route>
-          <Route path="attendance" >
-            <Route index element={<RequireAuth><Attendance /></RequireAuth>} />
-            <Route path="new" element={<RequireAuth><NewAttendance /></RequireAuth>} />
-            <Route path="delete" element={<RequireAuth><DeleteAttendance /></RequireAuth>} />
-          </Route >
-          <Route path="behaviour">
-            <Route index element={<RequireAuth><Behaviour /></RequireAuth>} />
-            <Route path="new/:id" element={<RequireAuth><NewBehaviour /></RequireAuth>} />
-            <Route path="view/:id" element={<RequireAuth><ViewBehaviour /></RequireAuth>} />
-            <Route path="edit/:id" element={<RequireAuth><EditBehaviour /></RequireAuth>} />
-          </Route>
-        </Route>
-        <Route path="login" element={<NoRequireAuth><Login /></NoRequireAuth>} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-    </>
+    <AppRoutes />
   )
 }
 
 export default App
-
-App.propTypes = {
-  children: PropTypes.node
-}
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,79 @@
+import { Route, Routes, Navigate } from "react-router-dom"
+import { useAuthContext } from "../hooks/useAuthContext"
+import Layout from "../components/layout/Layout"
+import Dashboard from "../pages/dashboard/Dashboard"
+import Profile from "../pages/profile/Profile"
+import NewProfile from "../pages/newProfile/NewProfile"
+import Classes from "../pages/classes/Classes"
+import NewClass from "../pages/newClass/NewClass"
+import EditClass from "../pages/editClass/EditClass"
+import Students from "../pages/students/Students"
+import NewStudent from "../pages/newStudent/NewStudent"
+import EditStudent from "../pages/editStudent/EditStudent"
+import ViewStudent from "../pages/viewStudent/ViewStudent"
+import Attendance from "../pages/attendance/Attendance"
+import NewAttendance from "../pages/newAttendance/NewAttendance"
+import DeleteAttendance from "../pages/deleteAttendance/DeleteAttendance"
+import Behaviour from "../pages/behaviour/Behaviour"
+import NewBehaviour from "../pages/newBehaviour/NewBehaviour"
+import ViewBehaviour from "../pages/viewBehaviour/ViewBehaviour"
+import EditBehaviour from "../pages/editBehaviour/EditBehaviour"
+import Login from "../pages/login/Login"
+import NotFound from "../pages/notFound/NotFound"
+import PropTypes from "prop-types"
+
+function AppRoutes() {
+
+    const { user } = useAuthContext()
+    const RequireAuth = ({ children }) => {
+        return user ? children : <Navigate to="/login" />
+    }
+    const NoRequireAuth = ({children}) => {
+        return !user ? children : <Navigate to="/" />
+    }
+    const AdminAuth = ({ children }) => {
+        return user?.user.role == "Admin" ? children : <Navigate to="/profile" />
+    }
+
+    return (
+        <Routes>
+            <Route path="/" element={<RequireAuth><Layout /></RequireAuth>}>
+                <Route index element={<RequireAuth><Dashboard /></RequireAuth>} />
+                <Route path="profile" >
+                <Route index element={<RequireAuth><Profile /></RequireAuth>} />
+                <Route path="new" element={<AdminAuth><NewProfile /></AdminAuth>} />
+                </Route>
+                <Route path="classes" >
+                <Route index element={<RequireAuth><Classes/></RequireAuth>} />
+                <Route path="new" element={<RequireAuth><NewClass /></RequireAuth>} /> 
+                <Route path="edit/:name" element={<RequireAuth><EditClass /></RequireAuth>} /> 
+                </Route>
+                <Route path="students" >
+                <Route index element={<RequireAuth><Students /></RequireAuth>} />
+                <Route path="new" element={<RequireAuth><NewStudent /></RequireAuth>} />
+                <Route path="view/:id" element={<RequireAuth><ViewStudent /></RequireAuth>} />
+                <Route path="edit/:id/:code" element={<RequireAuth><EditStudent /></RequireAuth>} />
+                </Route>
+                <Route path="attendance" >
+                <Route index element={<RequireAuth><Attendance /></RequireAuth>} />
+                <Route path="new" element={<RequireAuth><NewAttendance /></RequireAuth>} />
+                <Route path="delete" element={<RequireAuth><DeleteAttendance /></RequireAuth>} />
+                </Route >
+                <Route path="behaviour">
+                <Route index element={<RequireAuth><Behaviour /></RequireAuth>} />
+                <Route path="new/:id" element={<RequireAuth><NewBehaviour /></RequireAuth>} />
+                <Route path="view/:id" element={<RequireAuth><ViewBehaviour /></RequireAuth>} />
+                <Route path="edit/:id" element={<RequireAuth><EditBehaviour /></RequireAuth>} />
+                </Route>
+            </Route>
+            <Route path="login" element={<NoRequireAuth><Login /></NoRequireAuth>} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    )
+}
+
+export default AppRoutes
+
+AppRoutes.propTypes = {
+    children: PropTypes.node
+}
